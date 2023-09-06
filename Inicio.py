a34bdfb0d331ce36142ff2d91eaaaffaae16e217
yuribelentani <67803851+yuribelentani@users.noreply.github.com>
2023-09-05 22:32:46 -0300

    Add files via upload

---
+++ Inicio.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.sidebar.success('Selecione a página que gostaria de entrar')
+st.title('Ferramenta para Análise de Cenários de Investimento 💰💲📈')
+
+st.write('Esta ferramenta foi construída para auxiliar na análise de indicadores macroeconômicos e cenários de ativo.')
+
+
+st.markdown("- Macroeconomia")
+st.markdown("- Ações")
+st.markdown("- Renda Fixa")
+st.markdown("- Fundos Imobiliários")
